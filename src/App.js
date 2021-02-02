1aa2e79e3b40a7c6bb23bfb7967d19b733bf3d4e
Maya Rosmarin <mayarosmarin@Mayas-MacBook-Pro.local>
2021-02-01 22:07:02 -0500

    more images

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import Divider from '@material-ui/core/Divider';
 
 import './App.css'
 
@@ -9,17 +11,14 @@ const meals = [
         {
           name: "Sea Bass Ceviche with Tempura Onions",
           image: "sea_bass_ceviche",
-          date: "January 30th, 2021"
          },
          {
           name: "Chile de Agua Stuffed with Tuna Ceviche, Seared Tuna with Guajillo and Dried Shrimp Mole, Kalamata Puree ",
           image: "stuffed_chile",
-          date: ""
          },
          {
           name: "Sea Bass with New Mexican Pepper Adobo and Pineapple Puree",
           image: "sea_bass_al_pastor",
-          date: ""
          },
     ],
   },
@@ -29,7 +28,6 @@ const meals = [
       {
       name: "White Truffle Tajarin",
       image: "white_truffle_tajarin",
-      date: "December 15th, 2020"
       },
     ]
   },
@@ -39,22 +37,18 @@ const meals = [
       {
       name: "Mushroom Croquette with Shiitake Mushroom Cap, \"Watercress Turf\", Watercress Puree, Edible Flowers",
       image: "mushroom_croquette",
-      date: "December 13th, 2020"
      },
      {
       name: "Caramelized Artichoke Hearts, Sunchoke and Artichoke Puree, Creme Artichaut, \"Artichoke Essence\", Baby Greens",
       image: "caramelized_artichoke_hearts",
-      date: ""
      },
      {
       name: "Sea Bream with \"Potato Scales\", Fried Capers, Candied Meyer Lemon Slices and Peel, Roasted Caper Cream, and Parley",
       image: "sea_bream_with_potato",
-      date: ""
      },
      {
       name: "Chocolate Cinnamon Veloute with Chocolate Sauce, Cinnamon Stick Ice Cream, Cinnamon Cookie",
       image: "chocolate_veloute",
-      date: ""
      },
     ]
   },
@@ -64,22 +58,18 @@ const meals = [
      {
       name: "Salmon Tartare Cornets with Shallot Creme Fraiche and Chives",
       image: "salmon_cornets",
-      date: "November 15th, 2020"
      },
      {
       name: "Gazpacho with Balsamic Glaze",
       image: "gazpacho",
-      date: ""
      },
      {
       name: "Smoked Salmon with Gnocchi, Radicchio, Chive Oil, Balsamic Glaze",
       image: "smoked_salmon_gnocchi",
-      date: ""
      },
      {
       name: "\"Ille Flottante\" - Meringue Filled with Chocolate Mousse, Chocolate Shavings, Mint Oil",
       image: "ille_flottante",
-      date: ""
      },
     ]
   },
@@ -89,9 +79,34 @@ const meals = [
        {
         name: "Beef Wellington",
         image: "beef_wellington",
-        date: "November 1st, 2020"
        },
      ],
+  },
+  {
+    date: "October 11th, 2020",
+    courses: [
+        {
+            name: "Duck Fat Roasted Carrots with Carrot Puree, Date Puree, Carrot Duck Crumble, Wheat Berries, Pickled Dates, and Carrot Sauce",
+            image: "carrot_on_carrot_on_carrot",
+        },
+        {
+            name: "Branzino, Roasted Figs, Port-Soaked Dried Figs, Roasted Shiitake, Sweet Potato Puree, Serrano Ham, and Spinach",
+            image: "branzino_roasted_figs",
+        },
+        {
+            name: "Chocolate-Tahini Feuilletine, Chocolate Sesame Mousse, Black Sesame Paste, Yuzu Pate de Fruit, Brulee Banana, and Roasted Banana Sorbet",
+            image: "banana_sesame_tahini",
+        },
+    ],
+  },
+  {
+    date: "October 4th, 2020",
+    courses: [
+        {
+            name: "Lavender Duck with Roasted Carrots and Sumac Cream and Pickled Daikon",
+            image: "lavender_duck",
+        },
+    ],
   }
 ];
 
@@ -103,11 +118,11 @@ function Meal(props) {
               <br />
               <name>{course.name}</name>
               <br />
-              <br />
-              <img src={`${process.env.PUBLIC_URL}/assets/images/${course.image}.jpg`} />
-              <br />
+              <LazyLoadImage src={`${process.env.PUBLIC_URL}/assets/images/${course.image}.jpg`} />
             </div>
           ))}
+     <br />
+     <Divider variant="middle" />
      </div> 
    ) 
 }
@@ -116,10 +131,8 @@ function App() {
     return (
         <div className="body">
           <header>Matt and Maya Cook</header>
-          {meals.map(meal => (
+            {meals.map(meal => (
             <div>
-             <br />
-             <br />
             <date>{meal.date}</date>
             <Meal courses={meal.courses} />
             </div>
