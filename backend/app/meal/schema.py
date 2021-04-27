b3b7cde77fe6c2f8ed1e5d7bda9882b6e392edb3
Maya Rosmarin <maya.rosmarin@gmail.com>
2021-04-27 19:30:44 -0400

    backend

---
+++ backend/app/meal/schema.py
@@ -0,0 +1,11 @@
+from marshmallow import Schema, fields
+
+class MealSchema(Schema):
+  id = fields.Int(required=True)
+  date = fields.Str()
+
+class CourseSchema(Schema):
+  id = fields.Int(required=True)
+  name = fields.Str()
+  image = fields.Str()
+  meal = fields.Nested(MealSchema)
